d87d9e94d636f5997e7c49c044eaffab165a118d
kartik <kavimandankartik33@gmail.com>
2021-03-02 08:39:31 -0800

    Added: working Hr of employe

--- empwageComputation.sh
+++ empwageComputation.sh
@@ -1,18 +1,28 @@
-#!/bin/bash -x
-ispartTime=1
-isfullTime=2
-empRatePerHr=20
-empcheck=$((RANDOM%3))
+#! /bin/bash
 
-case $empcheck in
-		$isfullTime)
-			empHr=8
-;;
-		$ispartTime)
-		empHr=4
-;;
-*)
-		empHr=0
-;;
-esac
-salary=$(($empHr*$empRatePerHr))
+isPartTime=1;
+isFullTime=2;
+totalSalary=0;
+empRatePerHr=20;
+numWorkingDays=20;
+
+for(( day=1; day<=$numWorkingDays; day++ ))
+do
+   empCheck=$((RANDOM%3))
+         case $empCheck in
+                  $isFullTime)
+                          empHrs=8
+                              ;;
+                     $isPartTime)
+                            empHrs=4
+                              ;;
+                   *)
+                   empHrs=0
+                        ;;
+      esac
+       
+        salary=$(( $empHrs*$empRatePerHr )) 
+        totalSalary=$(( $totalSalary+$salary ))
+done
+echo  "Salary of an Employee" $totalSalary 
+
