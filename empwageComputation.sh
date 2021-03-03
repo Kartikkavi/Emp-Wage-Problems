7ec0b68ebd7eb6392e79a02bc8046a75fe304d05
Kartikkavi <78947510+Kartikkavi@users.noreply.github.com>
2021-03-03 19:08:05 +0530

    Update empwageComputation.sh

--- empwageComputation.sh
+++ empwageComputation.sh
@@ -1,28 +1,34 @@
-#! /bin/bash
+#!/bin/bash
 
-isPartTime=1;
-isFullTime=2;
-totalSalary=0;
-empRatePerHr=20;
-numWorkingDays=20;
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=100;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
 
-for(( day=1; day<=$numWorkingDays; day++ ))
+totalEmpHr=0;
+totalWorkingDays=0;
+
+while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]] 
 do
-   empCheck=$((RANDOM%3))
-         case $empCheck in
-                  $isFullTime)
-                          empHrs=8
-                              ;;
-                     $isPartTime)
-                            empHrs=4
-                              ;;
-                   *)
-                   empHrs=0
-                        ;;
-      esac
-       
-        salary=$(( $empHrs*$empRatePerHr )) 
-        totalSalary=$(( $totalSalary+$salary ))
-done
-echo  "Salary of an Employee" $totalSalary 
+  (( totalWorkingDays++ ))
+        workHours=$((RANDOM%3))
+         case $workHours in
+         $IS_FULL_TIME)
+          workHours=8
+                   ;;
+        $IS_PART_TIME)
+             workHours=4
+                   ;;
+      *)
+  workHours=0
+      ;;
+esac
+echo $workHours
+        totalWorkHours=$(($totalWorkHours+$workHours))
 
+ done
+
+totalSalary=$(($totalWorkHours*$EMP_RATE_PER_HR));
+
+echo "salary of an Employee= "$totalSalary
