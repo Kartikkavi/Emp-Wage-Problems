bd7f8814f27658f09d7041f9768c96f047d896c9
Kartikkavi <78947510+Kartikkavi@users.noreply.github.com>
2021-03-04 08:39:27 -0800

    Update empwageComputation.sh

--- empwageComputation.sh
+++ empwageComputation.sh
@@ -6,7 +6,7 @@ MAX_HRS_IN_MONTH=100;
 EMP_RATE_PER_HR=20;
 NUM_WORKING_DAYS=20;
 
-totalEWorkingHr=0;
+totalEWorkHours=0;
 totalWorkingDays=0;
 
 while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]] 
